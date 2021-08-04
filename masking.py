Fixed issue in masking.py where the images weren't the correct dimmensionality.

masking.py
@@ -16,7 +16,7 @@ def pipeline(im, fig_id=0):
     im_mean = np.mean(im)
     sobel = filters.sobel(im)
     
-    fig = plt.figure(fig_id, figsize=(12,6))
+    fig = plt.figure(fig_id, figsize=(12,4))
     ax1 = plt.subplot(141)
     ax2 = plt.subplot(142)
     ax3 = plt.subplot(143)
@@ -44,9 +44,9 @@ bse_paths = sorted(paths, key=lambda x: int(x[8:-4]))
 bse_imgs = np.zeros((391, 781, 1101))
 for i in range(len(bse_paths)):
     bse_paths[i] = os.path.join(bse_folder, bse_paths[i])
-    bse_imgs[i] = io.imread(bse_paths[i])
+    bse_imgs[i] = io.imread(bse_paths[i])[:,:,0]
 
-ebsd_imgs = ebsd['DataContainers/ImageDataContainer/CellData/Confidence Index'][...]
+ebsd_imgs = ebsd['DataContainers/ImageDataContainer/CellData/Confidence Index'][..., 0]
 
 pipeline(bse_imgs[0], 0)
 pipeline(ebsd_imgs[0], 1)