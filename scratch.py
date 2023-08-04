1fd45224984e18ccf2976b8d7835cf039dc71de5
James Lamb <jlamb@ucsb.edu>
2023-08-04 13:55:47 -0600

    Minor.

--- scratch.py
+++ scratch.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
@@ -6,6 +7,12 @@ from skimage import transform, io
 import core
 
 
-h = h5py.File("test_data/Test.dream3d", "r")
-print(h["DataStructure/DataContainer"].attrs.get("_SPACING"))
-h.close()
+path = "test_data/WCu/BSE/"
+files = sorted([f for f in os.listdir(path) if f.endswith(".tif")])
+print(files)
+
+for i, f in enumerate(files):
+    new_name = os.path.join(path, f"{i}.tif")
+    old_name = os.path.join(path, f)
+    print(old_name, ">", new_name)
+    # os.rename(old_name, new_name)
